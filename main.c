8d7ebb2ae25e809eb623c29beecdd33bf240594a
Ma3ert <yahyaitiazza15@gmail.com>
2025-03-22 14:50:23 +0000

    add comments

--- main.c
+++ main.c
@@ -19,23 +19,26 @@ int main(int argc, char **argv)
 	int		i;
 	bool	ipaddr;
 	t_input	input;
-
+	
+	i = 1;
+	ipaddr = false;
+	// init the input struct
 	input.scan = ALL_SCAN;
 	input.ipaddr = NULL;
 	input.port_range = 0;
 	input.port_start = 1;
 	input.thread_count = 0;
-	i = 1;
-	ipaddr = false;
 	// handles ./ft_nmap & ./ft_nmap --help
 	if (argc == 1 || (argc == 2 && !strcmp("--help", argv[1])))
 		print_help();
 	if (argc % 2 == 0)
 		print_help();
+	// loop through the args
 	while (argc > i)
 	{
-		if (!strcmp(argv[i], "--ip"))
+		if (!strcmp(argv[i], "--ip")) // this one handles "--ip"
 		{
+			// this one handle the duplication usage of --file and --ip
 			if (ipaddr)
 			{
 				printf("Error: only use --file or --ip\n");
@@ -45,8 +48,9 @@ int main(int argc, char **argv)
 				print_help();
 			ipaddr = true;
 		}
-		else if (!strcmp(argv[i], "--file"))
+		else if (!strcmp(argv[i], "--file")) // this one handles "--file"
 		{
+			// this one handle the duplication usage of --file and --ip
 			if (ipaddr)
 			{
 				printf("Error: only use --file or --ip\n");
@@ -56,13 +60,15 @@ int main(int argc, char **argv)
 				print_help();
 			ipaddr = true;
 		}
-		else if (!strcmp(argv[i], "--speedup") && parse_speedup(argv[i + 1], &input));
-		else if (!strcmp(argv[i], "--scan") && parse_scan(argv[i + 1], &input));
-		else if (!strcmp(argv[i], "--ports") && parse_ports(argv[i + 1], &input));
+		else if (!strcmp(argv[i], "--speedup") && parse_speedup(argv[i + 1], &input)); // this one handles "--speedup" pay attention to the &&
+		else if (!strcmp(argv[i], "--scan") && parse_scan(argv[i + 1], &input)); // this one handles "--scan" pay attention to the &&
+		else if (!strcmp(argv[i], "--ports") && parse_ports(argv[i + 1], &input)); // this one handles "--ports" pay attention to the &&
 		else
-			print_help();
-		i += 2;
+			print_help(); // handle mistaken args
+		// we skip the options value to get to the next key
+		i += 2; 
 	}
+	// checks if the required field is there
 	if (!ipaddr)
 		print_help();
 	printf("scan: %d / ports range: %d start from %d / threads: %d\n", input.scan, input.port_range, input.port_start, input.thread_count);
