1ea56346b73d8c5b01f8d3a322b07e1798bee245
Mohamed-Moumni <mmoumni@student.1337.ma>
2025-04-30 16:54:11 +0100

    feat: create the first version of syn scan

---
+++ scanner/syn_scan.c
@@ -0,0 +1,9 @@
+#include "ft_nmap.h"
+
+t_scan  *syn_scan(int port_number, int socket, t_ipaddr *ipaddr)
+{
+    // in this method i have to perform the SYN scan on this port number
+    // for this ip address.
+    // waiting for a response from the target.
+    
+}
