c41c7bdc1edc4e491c7485f1599ba85e958278d7
Mohamed-Moumni <mmoumni@student.1337.ma>
2025-05-13 08:56:52 +0100

    fix: fixed the logic of threads creation

--- network_mapper/nmap_loop.c
+++ network_mapper/nmap_loop.c
@@ -9,10 +9,17 @@ void nmap_loop(t_input *nmap_input)
 	t_list	*next_port_head;
 	int		offset;
 	int		step_count;
+	int		remainder;
 
 	nmap = NULL;
 	offset = 0;
-	step_count = (nmap_input->port_count / nmap_input->thread_count);
+	remainder = 0;
+	step_count = nmap_input->port_count <= nmap_input->thread_count ? 1 : nmap_input->port_count / nmap_input->thread_count;
+	if (nmap_input->port_count <= nmap_input->thread_count)
+		nmap_input->thread_count = nmap_input->port_count;
+	if (nmap_input->port_count > nmap_input->thread_count)
+		remainder = nmap_input->port_count % nmap_input->thread_count;
+
     while(nmap_input->ipaddr)
 	{
 		if (nmap_input->ipaddr->discovery)
@@ -31,7 +38,7 @@ void nmap_loop(t_input *nmap_input)
 
 				next_port_head = next_head_ports(nmap_input->ports, offset);
 				routine_arg.nmap = nmap_node;
-				routine_arg.port_range = step_count;
+				routine_arg.port_range = step_count + remainder;
 				routine_arg.scans = nmap_input->scans;
 				routine_arg.ports = next_port_head;
 
@@ -40,6 +47,7 @@ void nmap_loop(t_input *nmap_input)
 				if (error != 0)
 					print_error("Pthread Create: %s\n", strerror(error));
 				pthread_join(*thread, NULL);
+				remainder = remainder > 0 ? 0 : remainder;
 				offset += step_count;
 			}
 			list_free(&threads);
