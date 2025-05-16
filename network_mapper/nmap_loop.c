4bec3e662fd99d71369f0397b493bd45df140112
Mohamed-Moumni <mmoumni@student.1337.ma>
2025-05-16 10:48:27 +0100

    fix: fixed routine_arg free error

--- network_mapper/nmap_loop.c
+++ network_mapper/nmap_loop.c
@@ -15,7 +15,6 @@ void nmap_loop(t_input *nmap_input)
 	t_nmap	*nmap_node;
 	t_list	*nmap_list_node;
 	t_list	*threads;
-	t_list	*next_port_head;
 	int		offset;
 	int		step_count;
 	int		remainder;
@@ -24,7 +23,6 @@ void nmap_loop(t_input *nmap_input)
 
 	nmap = NULL;
 	services = service_mapper();
-	remainder = 0;
 	scan_counter = node_counter(nmap_input->scans);
 
 	if (nmap_input->thread_count)
@@ -35,6 +33,7 @@ void nmap_loop(t_input *nmap_input)
 		{
 			threads = NULL;
 			offset = 0;
+			remainder = 0;
 			nmap_node = create_nmap_node(nmap_input->ipaddr);
 			print_stats(nmap_node->ipaddr->ip_addr, nmap_input->port_count, nmap_input->scans, nmap_input->thread_count);
 			nmap_list_node = list_new(nmap_node, sizeof(t_list));
@@ -69,13 +68,12 @@ void nmap_loop(t_input *nmap_input)
 					t_list	*thread_node = list_new(thread, sizeof(pthread_t));
 					list_add(&threads, thread_node);
 					t_routine_arg *routine_arg;
-	
+
 					routine_arg = malloc(sizeof(t_routine_arg));
-					next_port_head = next_head_ports(nmap_input->ports, offset);
 					routine_arg->nmap = (t_nmap *)nmap_list_node->data;
 					routine_arg->port_range = step_count + remainder;
 					routine_arg->scans = nmap_input->scans;
-					routine_arg->ports = next_port_head;
+					routine_arg->ports = next_head_ports(nmap_input->ports, offset);
 					routine_arg->nmap->closed_ports = NULL;
 					routine_arg->nmap->open_ports = NULL;
 	
